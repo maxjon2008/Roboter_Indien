b4e78d59f4ed3371d12d4ab8017606639a83c610
maxjon2008 <maxjon@freenet.de>
2025-08-22 15:19:17 +0200

    Aray um zwei Datensätze per I2C zusenden

---
+++ I2C_Aray/Master_Code_Aray_mit_VL53L0X/Master_Code_Aray_mit_VL53L0X.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <VL53L0X.h>
+#include <roboter_master.h>
+
+int targetPulses_gesamt = 0; //Zielgeschwindigkeit von Roboter
+int targetPulses_Motor1 = 30; //Zielgeschwindigkeit von Motor1
+int targetPulses_Motor2 = 30; //Zielgeschwindigkeit von Motor2
+int targetPulses_Motor3 = 30; //Zielgeschwindigkeit von Motor3
+int targetPulses_Motor4 = 30; //Zielgeschwindigkeit von Motor4
+
+// serial data output interface (bluetooth)
+SoftwareSerial mySerial(5, 4);
+
+// VL53L0X Sensor
+VL53L0X TOF_sensor;
+
+void setup() {
+  Wire.begin();//0x05
+  Serial.begin(9600);
+
+  //Initialize the Serial Port for data output
+  mySerial.begin(9600);
+
+  // VL53L0X Sensor starten
+  TOF_sensor.init();
+  //TOF_sensor.setTimeout(500);
+
+  //Long Range Mode aktivieren
+  // TOF_sensor.setSignalRateLimit(0.1); // Standard 0.25 → kleinerer Wert = empfindlicher
+  // TOF_sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  // TOF_sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+
+  // Messzeit verlängern (Timing Budget)
+  // TOF_sensor.setMeasurementTimingBudget(200000); // 200ms pro Messung
+
+  Serial.println("VL53L0X Long Range Mode gestartet");
+  mySerial.println("VL53L0X Long Range Mode gestartet");  
+
+
+}
+
+void loop() {
+
+  //checkDistanceAndSetPulses_on_off(10); //10 P/0,1s bei on status
+  
+  sendtoSlave(0x08, -targetPulses_Motor1, 200);
+  sendtoSlave(0x09, targetPulses_Motor2, 200);
+  sendtoSlave(0x0A, -targetPulses_Motor3, 200);
+  sendtoSlave(0x0B, targetPulses_Motor4, 200);
+
+  delay(1000);
+  
+  sendtoSlave(0x08, targetPulses_Motor1, 200);
+  sendtoSlave(0x09, -targetPulses_Motor2, 200);
+  sendtoSlave(0x0A, targetPulses_Motor3, 200);
+  sendtoSlave(0x0B, -targetPulses_Motor4, 200);
+
+  delay(1000);
+}
---
+++ I2C_Aray/Slave_Code_Aray/Slave_Code_Aray.ino
@@ -0,0 +1,87 @@
+#include <Wire.h>
+#include "roboter_slave.h"
+
+#define SLAVE_ADRESSE 0x08 //change for every Slave (Motor1: 0x08; Motor2: 0x09; Motor3: 0x0A; Motor4: 0x0B)
+
+int Motor_A = 5;
+int Motor_B = 6;
+
+int encoder_Motor = 2;
+
+volatile int pulse_Motor = 0;
+volatile int gesammt_pulse = 0;
+
+unsigned long previousMillis = 0;
+long intervall = 100;
+
+//angekommene Werte:
+int max_pulse = 0;
+int targetPulses = 0;
+int16_t daten[2];
+
+int pwm_Motor = 0;
+
+
+void setup() {
+  pinMode(encoder_Motor,INPUT);
+  Serial.begin(9600);
+
+  attachInterrupt(digitalPinToInterrupt(encoder_Motor),counter_Motor,RISING);
+
+  Wire.begin(SLAVE_ADRESSE); 
+  Wire.onReceive(receiveEvent);
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+
+  if((currentMillis - previousMillis) >= intervall && gesammt_pulse <= max_pulse)
+  {
+    previousMillis = currentMillis;
+
+    Serial.println(targetPulses);
+    Serial.print("gesammt Pulse: ");
+    Serial.println(gesammt_pulse);
+
+    // === Regelung ===
+    pwm_Motor += 0.8 * (abs(targetPulses) - pulse_Motor);
+
+    // Begrenzen auf gültige Werte
+    pwm_Motor = constrain(pwm_Motor, 0, 255);
+
+    if(targetPulses > 0){ //Vorwärts fahren
+      Serial.print("v");
+      analogWrite(Motor_A, pwm_Motor);
+      analogWrite(Motor_B, 0);
+    }
+    else if(targetPulses < 0){ //Rückwärts fahren
+      Serial.print("r");
+      analogWrite(Motor_A, 0);
+      analogWrite(Motor_B, pwm_Motor);  
+    }
+    else{
+      Serial.print("0");
+      analogWrite(Motor_A, 0);
+      analogWrite(Motor_B, 0);   
+    }
+
+    Serial.print("Motor: P/0,1s: ");
+    Serial.println(pulse_Motor);
+    pulse_Motor = 0;
+  }
+
+  else if((currentMillis - previousMillis) >= intervall && gesammt_pulse >= max_pulse)
+  {
+
+    Serial.println("aus da max_pulse erreicht");
+    analogWrite(Motor_A, 0);
+    analogWrite(Motor_B, 0);
+
+    Serial.print("gesammt Pulse: ");
+    Serial.println(gesammt_pulse);
+  
+  }
+
+}
+
